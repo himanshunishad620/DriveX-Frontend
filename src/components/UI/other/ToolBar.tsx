d5b86f1873940176b0d28b3dafaba7ab78c58765
Himanshu Nishad <himanshunishad620@gmail.com>
2025-09-08 17:02:08 +0530

    sort toggle deprecated

--- src/components/UI/other/ToolBar.tsx
+++ src/components/UI/other/ToolBar.tsx
@@ -1,15 +1,15 @@
 import type { FileData } from "../../../types/ComponentsProps";
 import React, { useMemo, useState } from "react";
 import TextInput from "../reusable/TextInput";
-import IconButton from "../reusable/IconButton";
-import {
-  FcAlphabeticalSortingAz,
-  FcAlphabeticalSortingZa,
-  FcGenericSortingAsc,
-  FcGenericSortingDesc,
-  FcNumericalSorting12,
-  FcNumericalSorting21,
-} from "react-icons/fc";
+// import IconButton from "../reusable/IconButton";
+// import {
+//   FcAlphabeticalSortingAz,
+//   FcAlphabeticalSortingZa,
+//   FcGenericSortingAsc,
+//   FcGenericSortingDesc,
+//   FcNumericalSorting12,
+//   FcNumericalSorting21,
+// } from "react-icons/fc";
 
 type Props = {
   setList: React.Dispatch<React.SetStateAction<FileData[]>>;
@@ -22,8 +22,8 @@ const ToolBar: React.FC<Props> = (props) => {
   // const [selectedIndex, setSelectedIndex] = useState<number>(0);
   const [selectedValue, setSelectedValue] = useState<string>("name");
   const [flagToReSort, setFlagToReSort] = useState<number>();
-  const [sortDirectionToggle, setSortDirectionToggle] =
-    useState<boolean>(false);
+  // const [sortDirectionToggle, setSortDirectionToggle] =
+  //   useState<boolean>(false);
 
   const handleSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFlagToReSort(Math.random() * 1000000000000);
@@ -62,38 +62,39 @@ const ToolBar: React.FC<Props> = (props) => {
       return 0; // default (no sort)
     });
 
-    if (sortDirectionToggle) {
-      sortedList.reverse();
-    }
+    // if (sortDirectionToggle) {
+    //   sortedList.reverse();
+    // }
     props.setList([...sortedList]);
     return sortedList;
-  }, [sortDirectionToggle, flagToReSort, selectedValue, props.setList]);
+    // }, [sortDirectionToggle, flagToReSort, selectedValue, props.setList]);
+  }, [flagToReSort, selectedValue, props.setList]);
 
   const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     // setSelectedIndex(e.target.selectedIndex);
     setSelectedValue(e.target.value);
   };
 
-  const handleSortDirectionToggle = () => {
-    setSortDirectionToggle(!sortDirectionToggle);
-  };
-  const SortButtonArray = [
-    <NameSortButton
-      handleSortDirectionToggle={handleSortDirectionToggle}
-      sortDirectionToggle={sortDirectionToggle}
-    />,
-    <SizeSortButton
-      handleSortDirectionToggle={handleSortDirectionToggle}
-      sortDirectionToggle={sortDirectionToggle}
-    />,
-    <DateSortButton
-      handleSortDirectionToggle={handleSortDirectionToggle}
-      sortDirectionToggle={sortDirectionToggle}
-    />,
-  ];
+  // const handleSortDirectionToggle = () => {
+  //   setSortDirectionToggle(!sortDirectionToggle);
+  // };
+  // const SortButtonArray = [
+  //   <NameSortButton
+  //     handleSortDirectionToggle={handleSortDirectionToggle}
+  //     sortDirectionToggle={sortDirectionToggle}
+  //   />,
+  //   <SizeSortButton
+  //     handleSortDirectionToggle={handleSortDirectionToggle}
+  //     sortDirectionToggle={sortDirectionToggle}
+  //   />,
+  //   <DateSortButton
+  //     handleSortDirectionToggle={handleSortDirectionToggle}
+  //     sortDirectionToggle={sortDirectionToggle}
+  //   />,
+  // ];
   return (
     <div className="flex w-full items-center justify-between">
-      <div className="w-50 md:w-70">
+      <div className="w-60 md:w-70">
         <TextInput
           parentWidth={true}
           placeHolder="Search by name"
@@ -136,78 +137,78 @@ const ToolBar: React.FC<Props> = (props) => {
   );
 };
 
-type DateSortButtonProps = {
-  sortDirectionToggle: boolean;
-  handleSortDirectionToggle: () => void;
-};
+// type DateSortButtonProps = {
+//   sortDirectionToggle: boolean;
+//   handleSortDirectionToggle: () => void;
+// };
 
-const DateSortButton: React.FC<DateSortButtonProps> = (props) => {
-  return (
-    <div className="flex items-center justify-center">
-      {props.sortDirectionToggle ? (
-        <IconButton
-          icon={FcNumericalSorting12}
-          iconColor="text-gray-500"
-          onClick={props.handleSortDirectionToggle}
-        />
-      ) : (
-        <IconButton
-          icon={FcNumericalSorting21}
-          iconColor="text-gray-500"
-          onClick={props.handleSortDirectionToggle}
-        />
-      )}
-    </div>
-  );
-};
+// const DateSortButton: React.FC<DateSortButtonProps> = (props) => {
+//   return (
+//     <div className="flex items-center justify-center">
+//       {props.sortDirectionToggle ? (
+//         <IconButton
+//           icon={FcNumericalSorting12}
+//           iconColor="text-gray-500"
+//           onClick={props.handleSortDirectionToggle}
+//         />
+//       ) : (
+//         <IconButton
+//           icon={FcNumericalSorting21}
+//           iconColor="text-gray-500"
+//           onClick={props.handleSortDirectionToggle}
+//         />
+//       )}
+//     </div>
+//   );
+// };
 
-type NameSortButtonProps = {
-  sortDirectionToggle: boolean;
-  handleSortDirectionToggle: () => void;
-};
+// type NameSortButtonProps = {
+//   sortDirectionToggle: boolean;
+//   handleSortDirectionToggle: () => void;
+// };
 
-const NameSortButton: React.FC<NameSortButtonProps> = (props) => {
-  return (
-    <div className="flex items-center justify-center">
-      {!props.sortDirectionToggle ? (
-        <IconButton
-          icon={FcAlphabeticalSortingAz}
-          iconColor="text-gray-500"
-          onClick={props.handleSortDirectionToggle}
-        />
-      ) : (
-        <IconButton
-          icon={FcAlphabeticalSortingZa}
-          iconColor="text-gray-500"
-          onClick={props.handleSortDirectionToggle}
-        />
-      )}
-    </div>
-  );
-};
+// const NameSortButton: React.FC<NameSortButtonProps> = (props) => {
+//   return (
+//     <div className="flex items-center justify-center">
+//       {!props.sortDirectionToggle ? (
+//         <IconButton
+//           icon={FcAlphabeticalSortingAz}
+//           iconColor="text-gray-500"
+//           onClick={props.handleSortDirectionToggle}
+//         />
+//       ) : (
+//         <IconButton
+//           icon={FcAlphabeticalSortingZa}
+//           iconColor="text-gray-500"
+//           onClick={props.handleSortDirectionToggle}
+//         />
+//       )}
+//     </div>
+//   );
+// };
 
-type SizeSortButtonProps = {
-  sortDirectionToggle: boolean;
-  handleSortDirectionToggle: () => void;
-};
+// type SizeSortButtonProps = {
+//   sortDirectionToggle: boolean;
+//   handleSortDirectionToggle: () => void;
+// };
 
-const SizeSortButton: React.FC<SizeSortButtonProps> = (props) => {
-  return (
-    <div className="flex items-center justify-center">
-      {!props.sortDirectionToggle ? (
-        <IconButton
-          icon={FcGenericSortingAsc}
-          iconColor="text-gray-500"
-          onClick={props.handleSortDirectionToggle}
-        />
-      ) : (
-        <IconButton
-          icon={FcGenericSortingDesc}
-          iconColor="text-gray-500"
-          onClick={props.handleSortDirectionToggle}
-        />
-      )}
-    </div>
-  );
-};
+// const SizeSortButton: React.FC<SizeSortButtonProps> = (props) => {
+//   return (
+//     <div className="flex items-center justify-center">
+//       {!props.sortDirectionToggle ? (
+//         <IconButton
+//           icon={FcGenericSortingAsc}
+//           iconColor="text-gray-500"
+//           onClick={props.handleSortDirectionToggle}
+//         />
+//       ) : (
+//         <IconButton
+//           icon={FcGenericSortingDesc}
+//           iconColor="text-gray-500"
+//           onClick={props.handleSortDirectionToggle}
+//         />
+//       )}
+//     </div>
+//   );
+// };
 export default ToolBar;
