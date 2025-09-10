50e2b1ba977fb45e9c17c1c934fafa2a11fa1a93
Himanshu Nishad <himanshunishad620@gmail.com>
2025-09-10 11:06:04 +0530

    resume added

--- src/components/UI/other/AboutMe.tsx
+++ src/components/UI/other/AboutMe.tsx
@@ -37,7 +37,9 @@ const AboutMe: React.FC = () => {
             it as a showcase of my ability to design, develop, and deliver
             complete applications from scratch.
           </i>
-          <Button label="Resume" size="md" icon={FaFile} />
+          <a href="https://backend-u36p.onrender.com/api/file/download?url=https://res.cloudinary.com/do0rqy4oy/raw/upload/v1757482447/eez5nhxjozva0uegeyll&fileName=Himanshu_Nishad_Frontend_Developer_Resume.pdf">
+            <Button label="Resume" size="md" icon={FaFile} />
+          </a>
         </div>
       </div>
     </section>
